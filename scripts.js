071c804596234021abf0593b82c1bad70c5e41bf
Tatiana Nerucheva <tatiana@nerucheva.com>
2021-04-27 15:17:20 +0200

    Change the appearance of elements - usability howto: flex-direction column, fix paddings and margins - menu: fix padding-top

--- scripts.js
+++ scripts.js
@@ -2,12 +2,12 @@ const navLinks = document.querySelectorAll(".nav-link");
 const toggler = document.querySelector(".navbar-toggler");
 const collapsible = document.querySelectorAll(".collapsible-item");
 
-navLinks.forEach((l) => {
-  l.addEventListener("click", () => {
-    navLinks.forEach((e) => e.classList.remove("active"));
-    l.classList.add("active");
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    navLinks.forEach((element) => element.classList.remove("active"));
+    link.classList.add("active");
 
-    collapsible.forEach((e) => e.classList.remove("show"));
+    collapsible.forEach((element) => element.classList.remove("show"));
     toggler.classList.add("collapsed");
   });
 });
